24d4dc917d64a741fb8896d526a105191a39e054
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2019-07-24 00:54:41 +0800

    PersonalScene + AddedFolders
    -Added a Scripts Folder
        -Added a Mover Script for bullets
        -Added a Player Controller script
    
    -Added a Materials Folder
        -Added a grass placeholder material
    
    -Added a Prefabs folder
        -Added a Player Subfolder
          -Added a Player/Gun prefab
          -Added a BasicShot prefab
        -Added an Enemy Subfolder
    
    -Set up a basic test scene for the defence shooter with controls for mouse + keyboard input.

--- GDS210.3 - Space-VR/Assets/Scripts/Mover.cs
+++ GDS210.3 - Space-VR/Assets/Scripts/Mover.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    //The speed the object will travel at.
     public float thrust;
+    //The lifespan of our object.
+    public float lifeSpan;
+    //A reference to our rigidbody.
     public Rigidbody myRigidBody;
     
     // Start is called before the first frame update
     void Start()
     {
+        //Gets our rigidbody component.
         myRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        myRigidBody.AddForce(transform.up * thrust);
+        //Moves the gameobject in a straight line.
+        myRigidBody.AddForce(transform.forward * thrust);
+        //Destroys our gameObject after it's lifespan.
+        Destroy(gameObject, lifeSpan);
     }
 }
--- GDS210.3 - Space-VR/Assets/Scripts/PlayerController.cs
+++ GDS210.3 - Space-VR/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //A reference to our rigidbody.
+    private Rigidbody myRigidBody;
+    //A reference to our main camera.
+    private Camera mainCamera;
+    //The length of our camera ray.
+    public float rayLength;
+
+    //A reference to our shot.
+    public GameObject shot;
+    //Where the shot will spawn.
+    public Transform shotSpawn;
+    //Our fire rate.
+    public float fireRate;
+    //A variable that helps with our fire rate.
+    private float nextFire;
+
     void Start()
     {
-        
+        //Gets our rigidbody component.
+        myRigidBody = GetComponent<Rigidbody>();
+        //Finds our camera in the scene.
+        mainCamera = FindObjectOfType<Camera>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MouseRaycast();
+
+        /*If we press the fire button and enough time has elapsed,
+         *since our last shot, calls the ShotSpawn function. 
+         */
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            ShotSpawn();
+        }
+    }
+
+    //Handles how our player rotates in the world.
+    void MouseRaycast()
+    {
+        //Draws a  ray to wherever the mouse is.
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        //The point in space we want our player to look at.
+        Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+        //Draws a blue line along our look direction in the shape and length of our ray.
+        Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
+
+        //Rotates our player to the space we want them to look at.
+        transform.LookAt(pointToLook);
+    }
+
+    //Handles how we shoot.
+    void ShotSpawn()
+    {
+        //Handles our shooting cooldown. Sets nextfire to Time.time + the fireRate.
+        nextFire = Time.time + fireRate;
+        //Spawns our shot at our spawners location and rotation.
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
     }
 }
