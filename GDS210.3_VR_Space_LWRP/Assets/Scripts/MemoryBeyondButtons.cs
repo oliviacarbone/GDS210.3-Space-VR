c73c25feceb6ae73988caa7c630a359eb785eb8f
RileyBain <43746710+RileyBain@users.noreply.github.com>
2019-07-25 20:49:07 +0800

    Memory game core mechanic
    A simon says demo that should provide a decent basis for the memory game

---
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/MemoryBeyondButtons.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryBeyondButtons : MonoBehaviour
+{
+    public Material lightMat;
+    public Material darkMat;
+
+    private Renderer rend;
+
+    public MemoryBeyondLogic mBL;
+
+    public int buttonNumber;
+    public delegate void ClickEv(int number);
+    public event ClickEv OnClick;
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+    }
+
+    private void OnMouseDown()
+    {
+        if (mBL.player)
+        {
+            ClickedColor();
+            OnClick.Invoke(buttonNumber);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        UnclickedColor();
+    }
+
+    public void ClickedColor()
+    {
+        rend.sharedMaterial = lightMat;
+    }
+
+    public void UnclickedColor()
+    {
+        rend.sharedMaterial = darkMat;
+    }        
+}
---
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/MemoryBeyondLogic.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MemoryBeyondLogic : MonoBehaviour
+{
+    public MemoryBeyondButtons[] buttons;
+    public List<int> colorList;
+
+    public float hLTime = 0.5f;
+    public float delayTime = 0.5f;
+
+    public int level = 2;
+    public int playerLevel = 0;
+
+    public bool logic = false;
+    public bool player = false;
+
+    private int randomInt;
+
+    public Button startButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].OnClick += ButtonClicked;
+            buttons[i].buttonNumber = i;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (logic)
+        {
+            logic = false;
+            StartCoroutine(GameLogic());
+        }
+    }
+
+    void ButtonClicked(int number)
+    {
+        if (player)
+        {
+            if (number == colorList[playerLevel])
+            {
+                playerLevel += 1;
+            }
+            else
+            {
+                GameOver();
+            }
+            if (playerLevel == level)
+            {
+                level += 1;
+                playerLevel = 0;
+                player = false;
+                logic = true;
+            }
+        }
+    }
+
+    private IEnumerator GameLogic()
+    {
+        yield return new WaitForSeconds(1f);
+
+        for (int i = 0; i < level; i++)
+        {
+            if (colorList.Count < level)
+            {
+                randomInt = Random.Range(0, buttons.Length);
+                colorList.Add(randomInt);
+            }
+            
+            buttons[colorList[i]].ClickedColor();
+            yield return new WaitForSeconds(hLTime);
+
+            buttons[colorList[i]].UnclickedColor();
+            yield return new WaitForSeconds(delayTime);
+        }
+        player = true;
+    }
+
+    public void StartGame()
+    {
+        logic = true;
+        playerLevel = 0;
+        level = 2;
+        startButton.interactable = false;
+    }
+
+    void GameOver()
+    {
+        startButton.interactable = true;
+        player = false;
+    }
+}
