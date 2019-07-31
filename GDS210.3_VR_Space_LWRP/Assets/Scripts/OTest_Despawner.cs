58aaae13140c0d8ea6f10573694cbc28a4491777
oliviacarbone <oliviatcarbone@gmail.com>
2019-07-31 19:50:20 +0800

    Implemented Spawning & DeSpawning Resouces with Force

---
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/OTest_Despawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OTest_Despawner : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Resource"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/OTest_Spawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OTest_Spawner : MonoBehaviour
+{
+
+    //Spawn this object
+    public GameObject spawnObject;
+
+    //public float maxTime = 3;
+    //public float minTime = 2;
+
+    //current time
+    private float time;
+
+    //The time to spawn the object
+    private float spawnTime = 1;
+
+    void Start()
+    {
+        //SetRandomTime();
+        //time = minTime;
+    }
+
+    void FixedUpdate()
+    {
+
+        //Counts up
+        time += Time.deltaTime;
+
+        //Check if its the right time to spawn the object
+        if (time >= spawnTime)
+        {
+            SpawnObject();
+            //SetRandomTime();
+        }
+
+    }
+
+
+    //Spawns the object and resets the time
+    void SpawnObject()
+    {
+        time = 0;
+        Instantiate(spawnObject, transform.position, spawnObject.transform.rotation);
+    }
+
+    //Sets the random time between minTime and maxTime
+    //void SetRandomTime()
+    //{
+    //    spawnTime = Random.Range(minTime, maxTime);
+    //}
+}
