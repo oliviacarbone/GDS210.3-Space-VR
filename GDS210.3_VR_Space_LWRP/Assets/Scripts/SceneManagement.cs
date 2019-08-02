fd54acfd51d0c9cf5f533eecbb13744278e16aa9
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2019-08-02 11:29:33 +0800

    Changes

--- GDS210.3_VR_Space_LWRP/Assets/Scripts/SceneManagement.cs
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/SceneManagement.cs
@@ -4,12 +4,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
-
+    //The way this work is when you want to call a certain scene,
+    //you will need to give it a number in the inspector to change scenes.
+    //So make sure that the build setting is se]t up properly.
     public void ChangeScene(int sceneIndex)
     {
         StartCoroutine(LoadAsync(sceneIndex));  
     }
 
+
+    //With loadScene, is makes unity stop everything and focus on chnaging scene.
+    //By doing this while playing VR, it changes the scene to an empty void before going, 
+    //to the new scene. While using a loadScene.Asynce, unity will do this in the background,
+    //so that you skip being in this empty void for a bit.
     IEnumerator LoadAsync (int sceneIndex)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
