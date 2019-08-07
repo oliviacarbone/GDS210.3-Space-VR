1465f690d5510af446e8c796ae23aa17482039f7
MacVP <48820988+MacVP@users.noreply.github.com>
2019-08-07 13:08:54 +0800

    Nav Mesh Update

---
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/Gun Mechanic/EnemyController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public GameObject[] posPoints;
+    private GameObject currentPoint;
+    private int index;
+    public NavMeshAgent agent;
+    public bool positioning;
+    public bool positioned;
+
+    void Start()
+    {
+        positioning = false;    
+    }
+
+    void Update()
+    {
+        //InvokeRepeating("RandomPoint", 2, 10);
+
+        if (positioning == false)
+        {
+            RandomPoint();
+        }      
+    }
+
+    void RandomPoint()
+    {
+        string destination;
+
+        index = Random.Range(0, posPoints.Length);
+        currentPoint = posPoints[index];
+        print (currentPoint.name);
+
+        destination = currentPoint.name;
+         
+        agent.SetDestination(currentPoint.transform.position);
+
+        positioning = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PositionPoint")
+        {
+            positioned = true;
+            positioning = false;
+            print("Positioned!");
+        }
+    }
+}
