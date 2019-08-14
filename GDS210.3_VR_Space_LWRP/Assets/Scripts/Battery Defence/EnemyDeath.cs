f0668ed38d36ee4222c9c25ec03c187975a265f5
LaserDawn84 <1011533@student.sae.edu.au>
2019-08-14 17:11:48 +0800

    Shooting Stuff

--- GDS210.3_VR_Space_LWRP/Assets/Scripts/Battery Defence/EnemyDeath.cs
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/Battery Defence/EnemyDeath.cs
@@ -8,44 +8,56 @@ public class EnemyDeath : MonoBehaviour
     public Gun playerGun;
     public float enemyHealth;
 
-    void Awake()
-    {
-
-    }
 
     void Start()
     {
         enemyHealth = 100;
     }
 
+    private void Update()
+    {
+        //enemyHealth = enemyHealth - 1f;
+
+        Death();
+    }
+
     public void Hit()
     {
         //Turns test drown red if hit 
-        renderColor.material.color = Color.red;
+        //renderColor.material.color = Color.red;
 
         //Calls ChangeColor() after 2 seconds
-        Invoke("ChangeColor", 2);
-
+        //Invoke("ChangeColor", 2);
+        Debug.Log("Hit Registered");
         Damage();
     }
 
     void ChangeColor()
     {
         //Turns Test drone back to white
-        renderColor.material.color = Color.white;
+        //renderColor.material.color = Color.white;
     }
 
     void Damage()
     {
+        
         //Damages enemy 
-        enemyHealth = enemyHealth - playerGun.playerDamage;
+        enemyHealth = enemyHealth - 50f; //playerGun.playerDamage;
+
+        Debug.Log("Target Damaged");
     }
 
 
     void Death()
     {
         //Destroys the enemy when health reaches 0
+
+        
         if(enemyHealth <= 0f)
+        {
+            Debug.Log("Destroyed");
             Destroy(gameObject);
+        }
+            
     }
 }
