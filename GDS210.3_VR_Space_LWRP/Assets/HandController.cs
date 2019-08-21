664d0d0d7cb6e2970da36a9f57d7297622dd2ebe
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2019-08-21 16:59:29 +0800

    HAND UPDOOT
    -Good shit from Karl boi

---
+++ GDS210.3_VR_Space_LWRP/Assets/HandController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    private Animator clawAnim;
+
+    [SerializeField]
+    private string animationName;
+
+    private void Awake()
+    {
+        clawAnim = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Pass a float between 0 and 0.99 to play the animation from that state, use the axis input from the triggers. MAKE SURE TO CLAMP IT!!;
+    /// </summary>
+    /// <param name="t"></param>
+    public void ScaleClawAnimation(float t)
+    {
+        clawAnim.speed = 1;
+        clawAnim.Play(animationName, -1, t);
+        clawAnim.speed = 0;
+    }
+}
