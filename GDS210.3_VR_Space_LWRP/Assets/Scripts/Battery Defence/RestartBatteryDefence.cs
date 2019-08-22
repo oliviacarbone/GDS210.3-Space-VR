eb5adfbe84f68c5e9872f6f933297692a6cc00ad
MacVP <1015235@student.sae.edu.au>
2019-08-22 20:55:55 +0800

    .

---
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/Battery Defence/RestartBatteryDefence.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Valve.VR;
+using UnityEngine.SceneManagement;
+
+public class RestartBatteryDefence : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean grabAction;
+    public GameObject startGameButton;
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "LeftController" || col.gameObject.tag == "RightController")
+        {
+
+            if (grabAction.GetLastStateDown(handType))
+            {
+                SceneManager.LoadScene("BatteryDefence");
+            }
+        }
+    }    
+    
+}
