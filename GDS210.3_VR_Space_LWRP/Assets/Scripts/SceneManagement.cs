32bf0aee0e915e6c4452b8a6f63ac48d3a195a36
Wildtigger <51219988+Wildtigger@users.noreply.github.com>
2019-08-23 09:40:34 +0800

    Scene Fader Implemented

--- GDS210.3_VR_Space_LWRP/Assets/Scripts/SceneManagement.cs
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/SceneManagement.cs
@@ -4,11 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    public SceneFader sceneFader;
+
+    void Start()
+    {
+        
+    }
     //The way this work is when you want to call a certain scene,
     //you will need to give it a number in the inspector to change scenes.
     //So make sure that the build setting is se]t up properly.
     public void ChangeScene(int sceneIndex)
     {
+        StartCoroutine(GameObject.FindObjectOfType<SceneFader>().FadeAndLoadScene(SceneFader.FadeDirection.Out));
         Debug.Log("Changing Scene");
         StartCoroutine(LoadAsync(sceneIndex));  
     }
