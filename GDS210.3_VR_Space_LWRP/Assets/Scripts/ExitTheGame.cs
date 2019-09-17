62dbe06ba76ce4d568785b432a189646f948cbfd
Guillaume180 <43747099+Guillaume180@users.noreply.github.com>
2019-09-17 15:30:19 +0800

    c

---
+++ GDS210.3_VR_Space_LWRP/Assets/Scripts/ExitTheGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.SceneManagement;
+
+public class ExitTheGame : MonoBehaviour
+{
+
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean grabAction;
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "LeftController" || col.gameObject.tag == "RightController")
+        {
+            if (grabAction.GetLastStateDown(handType))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
